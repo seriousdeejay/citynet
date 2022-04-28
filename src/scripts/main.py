486938f29bdd790c03a605e19cdbee326ab9d4de
seriousdeejay <seriousdeejay@gmail.com>
2022-04-28 20:51:38 +0200

    Created Virtual Environment

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,1 +1,16 @@
 # Main Script
+
+import bs4
+import requests
+
+response = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
+
+if response is not None:
+    page = bs4.BeautifulSoup(response.text, 'html.parser')
+
+    title = page.select('#firstHeading')[0].text
+    paragraphs = page.select('p')
+    
+    print(title)
+    intro = '\n'.join([ paragraph.text for paragraph in paragraphs[0:5]])
+    print(intro)
