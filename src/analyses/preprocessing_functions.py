80e70ca2a9362ad75f12a10409b1d398f006ffbf
Brecht Nijman <brecht.nijman@gmail.com>
2022-05-18 00:11:22 +0200

    moved functions from selecting_relevant_files notebook to separate script preprocessing_functions.py

---
+++ src/analyses/preprocessing_functions.py
@@ -0,0 +1,298 @@
+import re
+import pandas as pd
+import os
+import time
+from tqdm.notebook import tqdm
+import unidecode
+
+# define functions for extracting metadata
+def find_title(input_string):
+    """function that returns the title of an article"""
+    reg_str = "title=\"" + "(.*?)" + "\""
+    title = re.findall(reg_str, input_string)
+    return title[0]
+
+def find_id(input_string):
+    """function that returns the id of an article"""
+    reg_str = "id=\"" + "(.*?)" + "\""
+    article_id = re.findall(reg_str, input_string)
+    return article_id[0]
+
+def find_url(input_string):
+    """function that returns the url of an article"""
+    reg_str = "url=\"" + "(.*?)" + "\""
+    url = re.findall(reg_str, input_string)
+    return url[0]
+
+def find_article(input_string):
+    """function that returns the body of an article"""
+
+    everything_str = r".*?"
+    reg_str = "<doc id=\"" + everything_str + "\">([\S\s]*?)<\/do"
+
+    article = re.findall(reg_str, input_string)
+
+    return article[0]
+
+
+# function that checks if key words in corpus
+def list_in_corpus(list_of_words, text_corpus, n = 2):
+    """
+    function that checks whether n number of words from a key word list occur in a given corpus.
+
+        Parameters:
+        -----------
+            list_of_words: (list, str) contains the key words to be matched
+            text_corpus:   (str)
+            n:             (int, optional)
+                minimum number of words from list_of_words that must appear in text_corpus.
+                default is 2.
+    """
+    inclusion = False
+    count = 0
+    for word in list_of_words:
+        if count < n: # only compare if inclusion condition has not yet been met
+            if word in text_corpus:
+                count += 1 # add 1 every time a city name is in the corpus
+        else:
+            pass
+    if count >= n:
+        inclusion = True
+        # thus the corpus is only marked for inclusion if
+        # at least n cities from the list have been mentioned
+    return inclusion
+
+
+# function to split dumps into flat list
+
+def split_dump(input_dump, split_pattern = "c>"):
+    """
+    splits list of wikidump documents into a flat list of articles
+
+        Parameters:
+        -----------
+            input_dump:    a list of strings
+            split_pattern: str, optional
+                string pattern at which the strings
+                should be split into articles. default = 'c>'
+
+    """
+
+    article_list = [
+        article for dump
+        in tqdm(input_dump, total = len(input_dump), desc = "Progress `split_dump()`")
+        for article in dump.split(split_pattern)]
+
+    return article_list
+
+# function to extract metadata from articles
+def process_dump(dump, key_words, message = True):
+    """extracts titles and ids from articles containing key words and returns as a list"""
+
+    articles = []
+    for article in tqdm(dump, total = len(dump), desc = "Progress"):
+        article = unidecode.unidecode(article)
+        if (list_in_corpus(key_words, article)):
+            try:
+                article_id = find_id(article)
+                title = find_title(article)
+                articles.append((article_id, title, article))
+            except:
+                pass
+        else:
+            pass
+
+    if message:
+        print(f"After processing {len(articles)} articles remain, "
+              f"that is {round(((len(articles)/len(dump))*100), 2)}% "
+              f"of the total number of articles ({len(dump)}) in this dump.")
+
+    return articles
+
+def process_dump2(dump, key_words, message = True):
+    """extracts titles and ids from articles containing key words and returns as a list"""
+
+    articles = []
+    for article in tqdm(dump, total = len(dump), desc = "Progress `process_dump()`:"):
+        article = unidecode.unidecode(article)
+        if (list_in_corpus(key_words, article)):
+            try:
+                article_id = find_id(article)
+                title = find_title(article)
+                article_body = find_article(article)
+                articles.append((article_id, title, article_body))
+            except:
+                pass
+        else:
+            pass
+
+    if message:
+        print(f"{round(((len(articles)/len(dump))*100), 2)}% of articles contain 2 toponyms")
+
+              # f"After processing {len(articles)} articles remain, "
+              # f"that is {round(((len(articles)/len(dump))*100), 2)}% "
+              # f"of the total number of articles ({len(dump)}) in this dump."
+    return articles
+
+def read_stream(indir):
+    """
+    Function to read all the files in a directory (or nested directories)
+    containing wikidump extracts.
+    Returns a list of strings each element representing the entire contents
+    of a single file
+
+        Parameters:
+        -----------
+        indir (str): path to a directory containing text files
+                     or directories with text files
+
+    """
+    wikidump = []
+    t0 = time.time()
+
+    for root, dirs, files in os.walk(indir):
+
+        for filename in files:
+            if not filename.startswith("."):
+                fp = os.path.join(root, filename)
+
+                with open(fp, 'r') as f:
+                    wikidump.append(f.read())
+
+    t1 = time.time()
+
+    total = t1-t0
+    print(f"It took {total}s to read {indir}.")
+
+    return wikidump
+
+# def write_outputcsv(df, outputfp, overwrite_protection = True):
+#     """
+#     function that writes a dataframe to a csv file. With added overwrite protection
+#     """
+#     # if the overwrite_protection variable is True a warning message will be displayed
+#     # and give the user the option to abort
+#     if overwrite_protection:
+#         if os.path.exists(outputfp):
+#             print(f"File {outputfp} already exists.")
+#             print("Are you sure you want to continue and overwrite the file?")
+#             decision = input('Continue? [y/n]')
+#             if decision == 'y':
+#                 df.to_csv(outputfp, index = False)
+#                 print(f"file has been written to: {outputfp}")
+#
+#                 if loop:
+#                     print("Do you want to perform the same action for the other files in the directory?")
+#                     loop_decision = input('Overwrite subsequent files? [y/n]')
+#                     if loop_decision == 'y':
+#                         print("Overwrite protection has been turned off.")
+#                         overwrite_protection = False
+#                     else:
+#                         print("Overwrite protection remains on.")
+#                 return overwrite_protection
+#
+#             elif decision == 'n':
+#                 print("The process has been halted.")
+#                 return
+#             else:
+#                 print("You did not enter a valid option.")
+#                 return
+#         else:
+#             df.to_csv(outputfp, index = False)
+#             print(f"file has been written to: {outputfp}")
+#             return
+#
+#     else:
+#         df.to_csv(outputfp, index = False)
+#         print(f"file has been written to: {outputfp}")
+#
+#     return
+
+def write_outputcsv(df, outputfp, overwrite_protection = True):
+    # if the overwrite_protection variable is True a warning message will be displayed
+    # and give the user the option to abort
+    if overwrite_protection:
+        if os.path.exists(outputfp):
+            print(f"File {outputfp} already exists.")
+            print("Are you sure you want to continue and overwrite the file?")
+            decision = input('Continue? [y/n]')
+
+            if decision == 'y':
+                df.to_csv(outputfp, index = False)
+                print(f"file has been written to: {outputfp}")
+            elif decision == 'n':
+                print("The process has been halted.")
+                return False
+            else:
+                print("You did not enter a valid option.")
+        else:
+            df.to_csv(outputfp, index = False)
+            print(f"file has been written to: {outputfp}")
+
+    else:
+        df.to_csv(outputfp, index = False)
+        print(f"file has been written to: {outputfp}")
+
+    return
+
+##############
+# function integrating the other functions
+def preprocess(base_dir, outdir, language, key_words, remove_referral=True, overwrite_protection=True):
+
+    # establish that a valid language was chosen:
+    lang_list = ['fr', 'en']
+    if language not in lang_list:
+        print(f"Invalid language was chosen. \n Please choose one of the following: {lang_list}")
+        return
+
+    # creating an output directory
+    outdir = os.path.join(outdir, f'{language}wiki/')
+#     outdir = f'../../../data/{language}wiki/'
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        print(f'created directory at: {outdir}')
+    else:
+        pass
+
+    # base input directory
+#     base_dir = f"/Volumes/NIJMAN/THESIS/{language}wiki_extracted" # path/to/wikidump/extracted
+
+    # list of multistream directories in base_dir
+    dir_list = os.listdir(base_dir)
+
+
+#     for directory in dir_list:
+    for directory in tqdm(dir_list, total = len(dir_list), desc = "Progress Total:"):
+        dir_fp = os.path.join(base_dir, directory)
+        print(f"Starting preprocessing on: {dir_fp}")
+        wikidump = read_stream(dir_fp) # read the files in the directory
+
+        wikidump = split_dump(wikidump) # split the files
+        wikidump = process_dump2(wikidump, key_words) # extract id, title, article
+
+        df = pd.DataFrame(wikidump, columns = ['article_id', 'title', 'text'])
+
+        if remove_referral:
+            try:
+                df['length'] = [len(text.split()) for text in df.text]
+                df['length_title'] = [len(title.split()) for title in df.title]
+                n_referral = len(df[df.length == df.length_title])
+
+                df = df[['article_id', 'title', 'text']][df.length != df.length_title]
+                print(f"Removing {n_referral} referral pages")
+
+            except:
+                print(f"Referral pages were not removed from multistream {directory}")
+                pass
+
+
+        # saving the output
+
+        outfile = f'{language}wikidump_{directory}.csv'
+        outputfp = os.path.join(outdir, outfile)
+
+        # call write_outputcsv function
+        write_outputcsv(df, outputfp, overwrite_protection = overwrite_protection)
+
+    return
