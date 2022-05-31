cf9b6b67fabc4737468fb9fc30db3cc19ee90295
seriousdeejay <seriousdeejay@gmail.com>
2022-05-31 17:44:46 +0200

    Updated diedfunctions

--- src/analyses/Diederik/diedfunctions.py
+++ src/analyses/Diederik/diedfunctions.py
@@ -1,11 +1,27 @@
-# Imports
-from gensim.corpora import Dictionary
+import pandas as pd # only required for old 'lemmatize' function
+
+# create_lemmatised_wordlists()
 from tqdm.notebook import tqdm
-import pandas as pd
 import spacy
 nlp = spacy.load("en_core_web_sm")
 import re
 
+# import_lemmatised_wordlists()
+import pickle
+import os
+
+#   vectorize()
+from gensim.corpora import Dictionary
+
+# initialize_wordcloud()
+import requests
+from io import BytesIO
+from PIL import Image
+from wordcloud import WordCloud
+
+
+
+### Can be removed in the near future
 def lemmatize(texts, POSfilter=["PROPN", "NOUN", "ADJ", "VERB", "ADV"], nlp_max_length = 1500000):
     """
     -->
@@ -51,8 +67,104 @@ def lemmatize(texts, POSfilter=["PROPN", "NOUN", "ADJ", "VERB", "ADV"], nlp_max_
 
 
 
+def create_lemmatised_wordlists(INPUT_DIR="../../../../data/enwiki_city_pairs/", OUTPUT_DIR= "../../../../data/NOUN/", POS=['NOUN']):
+    """
+    -->
+        function that function that lemmatises text files.
+
+        Parameters:
+        -----------
+            INPUT_PATH: str -> path to text files
+            OUTPUT_DIR: str -> path to lemmatised wordlists
+            POS: list (default = ['NOUN']) -> options: (https://spacy.io/usage/spacy-101#annotations-pos-deps)
+    """
+
+    #Checks if valid part-of-speech list was provided
+    if not isinstance(POS, list):
+        raise Exception("POS needs to be a list!")
+
+    POStags=["PROPN", "AUX", "NOUN", "ADJ", "VERB", "ADP", "SYM", "NUM"]
+    for tag in POS:
+        if tag.upper() not in POStags:
+            raise Exception(f'POSfilter only allows a list with one or multiple from the following tags: {POStags}.')
+   
+    for root, dirs, files in tqdm(list(os.walk(INPUT_DIR))):
+        
+        # Create subdirectories in output path
+        [os.makedirs(os.path.join(O, dir), exist_ok=True) for dir in dirs]
+
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_output_dir = root.replace(INPUT_DIR, OUTPUT_DIR)
+        
+            _lemmatize_file(file_path, file_output_dir, city_pair=file[:4], POS=POS)
+
+    return('Succesfully lemmatised the given texts.')
+
+
+
+def _lemmatize_file(FILE_PATH, OUTPUT_DIR, city_pair, POS):
+    """
+    -->
+        internal function that lemmatises a single file.
+
+        Parameters:
+        -----------
+            FILE_PATH: str -> path to text file
+            OUTPUT_DIR: str -> path to lemmatised wordlist
+            city_pair: str -> names of the two cities
+            POS: list (default = ['NOUN']) -> options: (https://spacy.io/usage/spacy-101#annotations-pos-deps)
+    """
+    with open(FILE_PATH, 'r', encoding='utf-16') as f:
+        city_pair_text_list = [x.strip().lower() for x in f.read().split('\n') if len(x) and 'title=' not in x]
+    print(len(city_pair_text_list))
+    
+    nr_of_chunks = len(city_pair_text_list)//1000 + 1
+    chunk_size = (len(city_pair_text_list)-1)//nr_of_chunks
+    chunked_text = [' '.join(city_pair_text_list[offs:offs+chunk_size]) for offs in range(0, len(city_pair_text_list), chunk_size)]
+    
+    processed_text = [text for text in tqdm(nlp.pipe(chunked_text, n_process=2, batch_size=1, disable=["ner", "parser"]), total=len(chunked_text))]
+    lemmatized_text = [[word.lemma_ for word in text if word.pos_ not in POS and not word.is_punct and not word.is_stop] for text in processed_text]
+    regexed_text = [[re.sub(r'\W+', '', word) for word in text] for text in lemmatized_text]
+    flattened_words = [item for sublist in regexed_text for item in sublist]
+    
+    with open(f'{OUTPUT_DIR}/{''.join(POS)}__{city_pair}__.pickle', 'wb') as fp:
+        pickle.dump(flattened_words, fp)
+
+
+
+def import_lemmatised_wordlists(PATH='../../../../data/enwiki_city_pairs_lemmatised/NOUN/', sort=True):
+    """
+    -->
+        function that imports (POS specific) wordlists belonging to specific city pairs.
+
+        Parameters:
+        -----------
+            PATH: str -> path to lemmatised wordlists
+            sort: bool (default = True) -> sort based on the number of words
+    """
+    
+    if not os.path.isdir(PATH):
+        raise Exception("Path is incorrect.")
+
+    data = []
+
+    for root, dirs, files in os.walk(PATH, topdown=True):
+        for name in files:
+            file_path = os.path.join(root, name)
+            
+            with open(file_path, 'rb') as fp:
+                data.append((pickle.load(fp), name.split('__')[1]))
+    
+    if sort:
+        data = sorted(data, key=lambda x: len(x[0]), reverse=True)
+
+    return data
+
+
+
 # Vectorization
-def vectorize(lemmatized_text, MIN_DF = 1, MAX_DF = 0.6):
+def vectorize(lemmatized_text, filter_extremes=True, MIN_DF = 1, MAX_DF = 0.6):
     """
     -->
         function that vectorizes preprocessed (lemmatized) text.
@@ -60,6 +172,7 @@ def vectorize(lemmatized_text, MIN_DF = 1, MAX_DF = 0.6):
         Parameters:
         -----------
             lemmatized_text: list, str -> contains the key words to be matched
+            filter_extremes: bool (default = True) -> whether you want to filter by min/max document frequency
             MIN_DF: int (default = 1) -> minimum document frequency
             MAX_DF: int (default = 0.6) -> maximum document frequency
 
@@ -67,8 +180,33 @@ def vectorize(lemmatized_text, MIN_DF = 1, MAX_DF = 0.6):
     
     # Get Vocabulary
     dictionary = Dictionary(lemmatized_text)
-    dictionary.filter_extremes(no_below=MIN_DF, no_above=MAX_DF)
+
+    if filter_extremes:
+        dictionary.filter_extremes(no_below=MIN_DF, no_above=MAX_DF)
     
     corpus = [dictionary.doc2bow(text) for text in lemmatized_text]
     
     return(dictionary, corpus)
+
+
+
+def initialize_wordcloud(background_color='#fff'):
+    """
+    -->
+        function that initialised a wordcloud.
+
+        Parameters:
+        -----------
+            background_color: hex (e.g. #fff) -> background color of the wordcloud
+    """
+
+    # black circle
+    response = requests.get('https://i.ibb.co/kHNWRYD/black-circle-better.png')
+    circle_mask = np.array(Image.open(BytesIO(response.content)))
+
+    wordcloud = WordCloud(background_color=background_color,
+                    # font_path='/System/Library/Fonts/Supplemental/DIN Alternate Bold.ttf',
+                    color_func=lambda *args, **kwargs: (0,0,0),
+                    mask=circle_mask)
+    
+    return wordcloud
